c88ddcfd946f6a0d5800f8e8d720414a8794c79e
Matthew Warman <warmanmr@gmail.com>
2024-04-10 16:45:27 -0400

    unit test configuration

---
+++ jest.setup.ts
@@ -0,0 +1,9 @@
+// declare env vars
+process.env.AWS_EXECUTION_ENV = 'execution-env';
+process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE = '1';
+process.env.AWS_LAMBDA_FUNCTION_NAME = 'function-name';
+process.env.AWS_LAMBDA_FUNCTION_VERSION = 'function-version';
+process.env.AWS_REGION = 'aws-region';
+process.env.AWS_APPCONFIG_APP_ID = 'aws-appconfig-application-id';
+process.env.AWS_APPCONFIG_ENV_ID = 'aws-appconfig-environment-id';
+process.env.AWS_APPCONFIG_PROFILE_ID = 'aws-appconfig-profile-id';
